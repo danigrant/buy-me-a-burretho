48315cac4e4c306b18ab5ae8933dd2f6bbb68423
dani <mayor@dani.town>
2019-09-06 15:59:17 -0400

    init

--- index.js
+++ index.js
@@ -3,15 +3,37 @@
 // <a class="doorkeeper-registration-widget" href="http://apicommunity.doorkeeper.jp/events/773">Register for Javascript Widget Demo</a>
 // script file and the element itself
 
+// user adds two script tags with two things in it - one is pulling this down and running it, other is variables for address and id to target
+
+const addr = '0x8d3e809Fbd258083a5Ba004a527159Da535c8abA'
+const domElementId = 'button-goes-here'
+
 document.addEventListener("DOMContentLoaded", main)
 
 async function main() {
   if (webThreeEnabled()) {
     // load the widget - get the element and add the contents
     // watch for a click
-    let button = document.getElementById('buy-me-a-burretho')
-    
+    addButtonToDOM()
+
   }
+}
+
+let addButtonToDOM = () => {
+  let el = document.getElementById(domElementId)
+  let button = document.createElement('div')
+  button.id = 'buy-me-a-burretho'
+  button.innerHTML = `<img src="" /><p>Buy me a burretho</p>`
+  el.insertBefore(button, el.firstChild)
+  addStyles()
+}
+
+let addStyles = () => {
+  let style = document.createElement('style');
+  document.head.appendChild(style);
+  style.sheet.insertRule(`@font-face { font-family: 'pacificoregular'; src: url('pacifico-regular-webfont.woff2') format('woff2'), url('pacifico-regular-webfont.woff') format('woff'); font-weight: normal; font-style: normal; }`)
+  style.sheet.insertRule(`#buy-me-a-burretho { color: darkseagreen; font-family: 'pacificoregular'; }`)
+
 }
 
 let webThreeEnabled = () => {
